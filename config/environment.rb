d524bc4effc0c1cabd7656777ee55842933d85f4
rngtng <tobi@rngntg.com>
2010-02-18 21:10:01 +0100

    added blueprint and styles extended layout

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem 'paperclip', :source => 'http://gemcutter.org'
 
-  Paperclip.options[:command_path] = "/usr/local/bin"
+ #  Paperclip.options[:command_path] = "/usr/local/bin"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
