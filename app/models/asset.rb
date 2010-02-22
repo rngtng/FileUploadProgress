4e8d1a02c705045beda69c3569f56cf09a68d3e1
Tobias Bielohlawek <tobiasb@qype.com>
2010-02-22 12:35:31 +0100

    fix

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,7 +5,7 @@ class Asset < ActiveRecord::Base
   validates_attachment_presence :file
 
   def self.generate_uuid
-     values = [ 0x0010000, 0x0010000, 0x0010000, 0x0010000, 0x0010000, 0x1000000, 0x1000000]map { |nr| rand(nr) }
+     values = [ 0x0010000, 0x0010000, 0x0010000, 0x0010000, 0x0010000, 0x1000000, 0x1000000].map { |nr| rand(nr) }
      "%04x%04x%04x%04x%04x%06x%06x" % values
    end
 end
